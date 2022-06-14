c5e39565906628fbdf64b99e2bae7dc4526d2558
Tony Chen <95876529+TonyC690@users.noreply.github.com>
2022-06-14 12:43:04 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,274 @@
+$(".yes").click(function() {
+    $(".page2").toggle();
+    $(".page1").toggle();
+});
+
+$(".no").click(function() {
+    $(".page1").toggle();
+    $(".gameover1").toggle();
+});
+
+$(".retry").click(function() {
+    $(".page1").show();
+    $(".gameover1").hide();
+});
+
+$(".no2").click(function() {
+    $(".page1").toggle();
+    $(".page2").toggle();
+});
+
+$(".yes2").click(function() {
+    $(".page2").hide();
+    $(".page3").show();
+});
+
+$(".yes3").click(function() {
+    $(".page4").toggle();
+    $(".page3").toggle();
+});
+
+$(".no3").click(function() {
+    $(".gameover1").toggle();
+    $(".page3").toggle();
+});
+
+$(".ew3").click(function() {
+    $(".page3extra").toggle();
+    $(".page3").toggle();
+});
+
+$(".extra3-2").click(function() {
+    $(".page3extra-1").toggle();
+    $(".page3extra").toggle();
+});
+
+$(".continue3").click(function() {
+    $(".wip").toggle();
+    $(".page3extra-1").toggle();
+});
+
+$(".wipimg").dblclick(function() {
+    $(".wip").toggle();
+    $(".page3").toggle();
+});
+
+$(".no4").click(function() {
+    $(".page3").toggle();
+    $(".page4").toggle();
+});
+
+$(".yes4").click(function() {
+    $(".page5").toggle();
+    $(".page4").toggle();
+});
+
+$(".yes5").click(function() {
+    $(".page6").toggle();
+    $(".page5").toggle();
+});
+
+$(".yes6").click(function() {
+    $(".page7").toggle();
+    $(".page6").toggle();
+});
+
+$(".yes7").click(function() {
+    $(".page8").toggle();
+    $(".page7").toggle();
+});
+
+$(".yes8").click(function() {
+    $(".page9").toggle();
+    $(".page8").toggle();
+});
+
+$(".yes9").click(function() {
+    $(".page10").toggle();
+    $(".page9").toggle();
+});
+
+$(".yes10").click(function() {
+    $(".page11").toggle();
+    $(".page10").toggle();
+});
+
+$(".yes11").click(function() {
+    $(".page12").toggle();
+    $(".page11").toggle();
+});
+                 
+$(".yes12").click(function() {
+    $(".page13").toggle();
+    $(".page12").toggle();
+});
+
+$(".yes13").click(function() {
+    $(".page14").toggle();
+    $(".page13").toggle();
+});
+
+$(".yes14").click(function() {
+    $(".page15").toggle();
+    $(".page14").toggle();
+});
+
+$(".end1").click(function() {
+    $(".end").toggle();
+    $(".page15").toggle();
+});
+
+$(".idk").click(function() {
+    $(".storyline").toggle();
+    $(".end").toggle();
+});
+
+$(".gif1").dblclick(function() {
+    $(".page1").show();
+    $(".gameover1").hide();
+});
+
+$(".extra3-1").click(function() {
+    $(".wip").show();
+    $(".page3extra").hide();
+});
+
+$(".no6").click(function() {
+    $(".scared").show();
+    $(".page6").hide();
+});
+
+$(".scaredimg").dblclick(function() {
+    $(".page6").show();
+    $(".scared").hide();
+});
+
+$(".no7").click(function() {
+    $(".dont").show();
+    $(".page7").hide();
+});
+
+$(".dontimg").dblclick(function() {
+    $(".page7").show();
+    $(".dont").hide();
+});
+
+$(".bad7").click(function() {
+    $(".wip1").show();
+    $(".page7").hide();
+});
+
+$(".wipimg1").dblclick(function() {
+    $(".wip1").hide();
+    $(".page7").show();
+});
+
+$(".oof8").click(function() {
+    $(".wip2").show();
+    $(".page8").hide();
+});
+
+$(".maybe8").click(function() {
+    $(".wip2").show();
+    $(".page8").hide();
+});
+
+$(".no8").click(function() {
+    $(".wip2").show();
+    $(".page8").hide();
+});
+
+$(".wipimg2").dblclick(function() {
+    $(".wip2").hide();
+    $(".page8").show();
+});
+
+$(".no9").click(function() {
+    $(".wip3").show();
+    $(".page9").hide();
+});
+
+$(".why9").click(function() {
+    $(".whytho").show();
+    $(".page9").hide();
+});
+
+$(".wipimg3").dblclick(function() {
+    $(".wip3").hide();
+    $(".page9").show();
+});
+
+$(".whythoimg").dblclick(function() {
+    $(".whytho").hide();
+    $(".page9").show();
+});
+
+$(".no10").click(function() {
+    $(".wip4").show();
+    $(".page10").hide();
+});
+
+$(".why10").click(function() {
+    $(".blind").show();
+    $(".page10").hide();
+});
+
+$(".wipimg4").dblclick(function() {
+    $(".wip4").hide();
+    $(".page10").show();
+});
+
+$(".blindimg").dblclick(function() {
+    $(".blind").hide();
+    $(".page10").show();
+});
+
+$(".no11").click(function() {
+    $(".lose1").show();
+    $(".page11").hide();
+});
+
+$(".no12").click(function() {
+    $(".learn1").show();
+    $(".page12").hide();
+});
+
+$(".loseimg1").dblclick(function() {
+    $(".lose1").hide();
+    $(".page11").show();
+});
+
+$(".learnimg1").dblclick(function() {
+    $(".learn1").hide();
+    $(".page12").show();
+});
+
+$(".no13").click(function() {
+    $(".lose2").show();
+    $(".page13").hide();
+});
+
+$(".loseimg2").dblclick(function() {
+    $(".lose2").hide();
+    $(".page13").show();
+});
+
+$(".no14").click(function() {
+    $(".death1").show();
+    $(".page14").hide();
+});
+
+$(".why14").click(function() {
+    $(".death2").show();
+    $(".page14").hide();
+});
+
+$(".deathimg1").dblclick(function() {
+    $(".death1").hide();
+    $(".page14").show();
+});
+
+$(".deathimg2").dblclick(function() {
+    $(".death2").hide();
+    $(".page14").show();
+});
